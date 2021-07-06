8820fc547770fbb08b0b1ebec541da8cfb8647b7
fab <fab@gmail.com>
2021-07-05 21:46:39 -0500

    quitando lineas innecesarias

--- resources/views/articulo/edit.blade.php
+++ resources/views/articulo/edit.blade.php
@@ -1,15 +1,15 @@
-@extends('layouts.plantillabase') //se extiende plantilla base 
+@extends('layouts.plantillabase') 
 
 @section('contenido')
 <h2>EDITAR REGISTROS</h2>
 
-<form action="/articulos/{{$articulo->id}}" method="POST"> //se redirige a articulos y despues se toma el id 
+<form action="/articulos/{{$articulo->id}}" method="POST"> 
     @csrf    
     @method('PUT')
   <div class="mb-3">
     <label for="" class="form-label">Código</label>
     <input id="codigo" name="codigo" type="text" class="form-control" value="{{$articulo->codigo}}">    
-  </div>                                                                     //trae el valor de la bd
+  </div>                                                                    
   <div class="mb-3">
     <label for="" class="form-label">Descripción</label>
     <input id="descripcion" name="descripcion" type="text" class="form-control" value="{{$articulo->descripcion}}">
